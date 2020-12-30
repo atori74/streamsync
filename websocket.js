c30ce20e70bcdcbf512d0563d77fe95bd47df6a2
Atori <x90.empty@outlook.com>
2020-12-31 03:14:35 +0900

    add websocket.js to handle websocket messages

---
+++ websocket.js
@@ -0,0 +1,17 @@
+const handleFrame = (obj) => {
+	// hoge
+	if(obj.from == 'server') {
+		switch(obj.type) {
+			case 'roomInfo':
+				// hoge
+				let roomId = obj.data.roomID;
+				console.log('roomID: ', roomId);
+				chrome.runtime.sendMessage({type: 'FROM_BG', command: 'roomInfo', data: {roomID: roomId}}, undefined);
+		}
+	}
+};
+
+const closeConnection = () => {
+	console.log('closeConnection was called')
+	chrome.runtime.sendMessage({type: 'FROM_BG', command: 'connectionClosed'});
+};
