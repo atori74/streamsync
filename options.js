2e08d8cd4dd1abb1cff4649f27bade89689dcf9b
Atori <x90.empty@outlook.com>
2021-03-25 17:18:56 +0900

    design option page

--- options.js
+++ options.js
@@ -1,17 +1,48 @@
 const loadVariables = async _ => {
+	console.log('reloaded')
 	const list = document.querySelector('div#options-list > dl.kv-list');
 	while(list.firstChild) { list.removeChild(list.lastChild) };
 	chrome.storage.local.get('env', data => {
 		const vars = data.env;
+		console.log(vars)
+		if(Object.keys(vars).length == 0) {
+			const key = document.createElement('dt');
+			key.innerText = 'no variables';
+			const value = document.createElement('dd');
+			value.innerText = '';
+
+			const row = document.createElement('div');
+			row.appendChild(key);
+			row.appendChild(value);
+
+			list.appendChild(row);
+		}
 		for (const k in vars) {
 			const key = document.createElement('dt');
 			key.innerText = k;
 			const value = document.createElement('dd');
 			value.innerText = vars[k];
 
+			const removeBtn = document.createElement('div');
+			removeBtn.className = 'kv-list-remove-row';
+			const removeImg = document.createElement('img');
+			removeImg.src = '/static/delete_black.png'
+			removeImg.className = 'remove-img'
+			removeBtn.appendChild(removeImg);
+
+			removeBtn.onclick = _ => {
+				chrome.storage.local.get('env', data => {
+					const vars = data.env;
+					delete vars[k];
+					chrome.storage.local.set({'env': vars}, undefined);
+					loadVariables();
+				})
+			}
+
 			const row = document.createElement('div');
 			row.appendChild(key);
 			row.appendChild(value);
+			row.appendChild(removeBtn);
 
 			list.appendChild(row);
 		}
